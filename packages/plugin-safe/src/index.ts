0bdf8cc8fb0c0d3674b365b0a368bd1357510184
Artur Shirokov <artur.wiseman.co@gmail.com>
2025-02-07 12:14:20 +0000

    Added my own search plugin and started working on the safe interaction plugin.

---
+++ packages/plugin-safe/src/index.ts
@@ -0,0 +1,14 @@
+import type { Plugin } from "@elizaos/core";
+
+import { executeTransaction } from "./actions/executeTransaction.ts";
+
+export * as actions from "./actions";
+
+export const bootstrapPlugin: Plugin = {
+  name: "safe",
+  description: "",
+  actions: [executeTransaction],
+  evaluators: [],
+  providers: [],
+};
+export default bootstrapPlugin;
