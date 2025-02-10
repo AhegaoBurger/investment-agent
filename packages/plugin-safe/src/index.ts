17195fcbbb637d76d4f9e1cd820162dc45960088
Artur Shirokov <artur.wiseman.co@gmail.com>
2025-02-10 16:45:06 +0000

    Check balances is done.

--- packages/plugin-safe/src/index.ts
+++ packages/plugin-safe/src/index.ts
@@ -1,14 +1,14 @@
 import type { Plugin } from "@elizaos/core";
 
 import { stake } from "./actions/stake.ts";
-
+import { checkPositions } from "./actions/checkBalance.ts";
 // export * as actions from "./actions";
 
 export const safePlugin: Plugin = {
   name: "safe",
   description:
     "Supplies/stakes tokens to the AAVE lending protocol to earn yield",
-  actions: [stake],
+  actions: [stake, checkPositions],
   evaluators: [],
   providers: [],
 };
